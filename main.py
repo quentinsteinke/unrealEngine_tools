716b2312362f2f4d7c2233e16ccff7fd646b57dc
QuentinOberon <quentin.steinke@oberontech.com>
2022-09-09 12:02:43 -0400

    update

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MeshComp(unreal.MeshComponent):
     pass
 
 
+############## Variables ##############
 selected_assets = EditorUtils().get_selected_assets()
 
 name = str(sys.argv[1])
@@ -19,6 +20,26 @@ prefix = str(sys.argv[3])
 addPrefix = str(sys.argv[4])
 setMaterial = str(sys.argv[5])
 smartApplyMaterials = str(sys.argv[6])
+# materialPath = str(sys.argv[7])
+
+
+############## Functoins ##############
+materialList = {}
+def get_all_materials():
+    asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_class("Material")
+    for asset in asset_list:
+        asset_name = str(asset.asset_name)
+        asset_path = str(asset.object_path)
+        if asset in materialList:
+            pass
+        else:
+            materialList[asset_name] = asset_path
+
+
+def get_asset_by_name(name: str, searchList: dict):
+    asset = (searchList[name])
+    print(f"returning asset {unreal.load_asset(asset)}")
+    return unreal.load_asset(asset)
 
 
 def testing():
@@ -32,28 +53,29 @@ def testing():
             print(f"It's class is type {asset}")
 
 
-def rename():
-    print(f"Name here: {name}")
+def rename(n: str=None):
+    for asset in selected_assets:
+        if n == None:
+            print(f"Name here: {name}")
+        else:
+            print(f"New name is {n}")
 
 
 def add_prefix():
-    print(f"addPrefix is {prefix}")
-
-
-def apply_material():
-    print("applying material")
+    for asset in selected_assets:
+        print(f"addPrefix is {prefix + asset.get_name()}")
 
 
 # Get last selected asset and return it
-def get_material():
-    material = selected_assets[-1]
+def get_last_selected():
+    last_selected = selected_assets[-1]
 
-    return material
+    return last_selected
 
 
 # Apply one material to slot one of all selelcted objects
 def set_material(slot):
-    material = get_material()
+    material = get_last_selected()
 
     for asset in selected_assets:
         if "StaticMesh" in str(asset.get_class()):
@@ -64,25 +86,35 @@ def set_material(slot):
 
 # Get material slot names, find matching named materials and apply to slot
 def smart_apply_materials():
-        for asset in selected_assets:
+    get_all_materials()
+    for asset in selected_assets:
+        if "StaticMesh" in str(asset.get_class()):
             sm_component = unreal.StaticMeshComponent()
             sm_component.set_static_mesh(asset)
             materialSlotNames = unreal.StaticMeshComponent.get_material_slot_names(sm_component)
-            for name in materialSlotNames:
-                material = unreal.AssetRegistry().get_assets_by_package_name(name)
-                print(material)
+            for x, matname in enumerate(materialSlotNames):
+                try:
+                    material = get_asset_by_name(str(matname), materialList)
+                    print(f"found {material}")
+                    asset.set_material(int(x), material)
+                except KeyError:
+                    print(f"Material '{matname}' not found on {asset.get_full_name()}")
         
 
 ############## Main Function Loop ##############
 def main():
-    if addName == "true":
+    if addName == "true" and addPrefix == "true":
+        newname = prefix + name
+        rename(newname)
+
+    elif addName == "true":
         rename()
 
-    if addPrefix == "true":
+    elif addPrefix == "true":
         add_prefix()
     
     if setMaterial == "true":
-        set_material()
+        set_material(0)
     
     if smartApplyMaterials == "true":
         smart_apply_materials()
