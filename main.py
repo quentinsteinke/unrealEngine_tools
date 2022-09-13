56c1571feb24c169955fb64342453451089e66fc
QuentinOberon <quentin.steinke@oberontech.com>
2022-09-13 16:52:14 -0400

    code clean up

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ import sys
 class EditorUtils(unreal.GlobalEditorUtilityBase):
     pass
 
-@unreal.uclass()
-class MeshComp(unreal.MeshComponent):
-    pass
-
 
 ############## Variables ##############
 selected_assets = EditorUtils().get_selected_assets()
 materialList = {}
 materialInstanceList = {}
 
+# Unreal Arguments
 name = str(sys.argv[1])
 addName = str(sys.argv[2])
 prefix = str(sys.argv[3])
@@ -23,50 +20,23 @@ addPrefix = str(sys.argv[4])
 setMaterial = str(sys.argv[5])
 smartApplyMaterials = str(sys.argv[6])
 materialType = str(sys.argv[7])
+# materialSlot = str(sys.argv[8])
 
 
 ############## Functoins ##############
-# for obj in selected_assets:
-#     print(obj.get_class())
-
-def testing():
-    for asset in selected_assets:
-        if asset.get_class() == "Object":
-            print(f"{asset.get_name()} is a Object")
-        elif asset.get_class() == "Material":
-            print(f"{asset.get_name()} is a Material")
-        else:
-            print(f"{asset.get_name()} is not a Material or an Object")
-
-
 def strip_prefix(name: str):
-    prefixes = [
-        "M",
-        "MI",
-        "SM",
-        "SKM",
-        "T",
-        "BP",
-    ]
+    prefixes = ["M", "MI", "SM", "SKM", "T", "BP",]
+    sliceNumber = 0
     
     try:
         splitName = name.split("_")
     except AttributeError:
         return name
 
-    sliceNumber = 0
-
     if splitName[0] in prefixes:
         sliceNumber = len(splitName[0]) + 1
     else:
         pass
-
-    # if splitName[0] == "M":
-    #     sliceNumber = len(splitName[0]) + 1
-    # elif splitName[0] == "MI":
-    #     sliceNumber = len(splitName[0]) + 1
-    # else:
-    #     pass
     
     name = name[sliceNumber:]
 
@@ -79,25 +49,16 @@ def get_all_materials():
         asset_name = strip_prefix(str(asset.asset_name)).upper()
         asset_path = str(asset.object_path)
         if asset not in materialList: materialList[asset_name] = asset_path
-        # if asset in materialList:
-        #     pass
-        # else:
-        #     materialList[asset_name] = asset_path
 
     material_instance_asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_class("MaterialInstanceConstant")
     for asset in material_instance_asset_list:
         asset_name = strip_prefix(str(asset.asset_name)).upper()
         asset_path = str(asset.object_path)
         if asset not in materialInstanceList: materialInstanceList[asset_name] = asset_path
-        # if asset in materialInstanceList:
-        #     pass
-        # else:
-        #     materialInstanceList[asset_name] = asset_path
 
 
 def get_asset_by_name(name: str, searchList: dict):
     asset = (searchList[name.upper()])
-    # print(f"returning asset {unreal.load_asset(asset)}")
     return unreal.load_asset(asset)
 
 
@@ -114,21 +75,18 @@ def add_prefix():
         print(f"addPrefix is {prefix + asset.get_name()}")
 
 
-# Get last selected asset and return it
 def get_last_selected():
     last_selected = selected_assets[-1]
 
     return last_selected
 
 
-# Apply one material to slot one of all selelcted objects
+# Apply one material to given slot for all selelcted objects
 def set_material(slot):
     material = get_last_selected()
 
     for asset in selected_assets:
         if "StaticMesh" in str(asset.get_class()):
-            print(asset.get_name())
-            # Change material
             asset.set_material(int(slot), material)
 
 
